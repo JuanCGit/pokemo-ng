2298503ee85f4568d8dbe7b0e666fcc3b3932693
JuanCGit <juancasanovagago@gmail.com>
2024-05-08 15:09:56 +0200

    Add home page component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', loadChildren: () => import('./pages/home-page/home-page.module').then((m) => m.HomePageModule) },
+    { path: '**', redirectTo: ''}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
