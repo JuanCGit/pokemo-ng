939e55d1547108c76bcecb6c67d11c733a8900b2
JuanCGit <juancasanovagago@gmail.com>
2024-05-23 16:03:28 +0200

    Refactor to banner

--- src/app/components/banner/banner.module.ts
+++ src/app/components/banner/banner.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { BannerComponent } from "./banner.component";
 import { ButtonModule } from "../button/button.module";
+import { ProgressBarModule } from "../progress-bar/progress-bar.module";
 
 @NgModule({
   declarations: [BannerComponent],
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, ProgressBarModule],
   exports: [BannerComponent],
 })
 export class BannerModule {}
