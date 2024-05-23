ce6cb930569aa3dbd0d868dfbcd09829f3d6253f
JuanCGit <juancasanovagago@gmail.com>
2024-05-23 16:57:59 +0200

    add 3d effect to card

--- src/app/components/overlay-image/overlay-image.component.ts
+++ src/app/components/overlay-image/overlay-image.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, Renderer2 } from "@angular/core";
 
 @Component({
   selector: "app-overlay-image",
@@ -6,8 +6,67 @@ import { Component } from "@angular/core";
   styleUrl: "./overlay-image.component.scss",
 })
 export class OverlayImageComponent {
+  private bounds!: DOMRect;
   imageUrl: string | null = null;
   backgroundImageUrl: string | null = null;
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+  ) {}
+
+  onMouseEnter(): void {
+    this.bounds = this.el.nativeElement
+      .querySelector(".card")
+      .getBoundingClientRect();
+    this.renderer.listen(
+      "document",
+      "mousemove",
+      this.rotateToMouse.bind(this),
+    );
+  }
+
+  onMouseLeave(): void {
+    this.renderer.removeClass(
+      this.el.nativeElement.querySelector(".card"),
+      "transformed",
+    );
+    this.el.nativeElement.querySelector(".card").style.transform = "";
+    this.el.nativeElement.querySelector(".glow").style.backgroundImage = "";
+    this.renderer.listen("document", "mousemove", () => {});
+  }
+
+  rotateToMouse(event: MouseEvent): void {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const leftX = mouseX - this.bounds.x;
+    const topY = mouseY - this.bounds.y;
+    const center = {
+      x: leftX - this.bounds.width / 2,
+      y: topY - this.bounds.height / 2,
+    };
+    const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+
+    this.el.nativeElement.querySelector(".card").style.transform = `
+      scale3d(1.07, 1.07, 1.07)
+      rotate3d(
+        ${center.y / 100},
+        ${-center.x / 100},
+        0,
+        ${Math.log(distance) * 2}deg
+      )
+    `;
+
+    this.el.nativeElement.querySelector(".glow").style.backgroundImage = `
+      radial-gradient(
+        circle at
+        ${center.x * 2 + this.bounds.width / 2}px
+        ${center.y * 2 + this.bounds.height / 2}px,
+        #ffffff55,
+        #0000000f
+      )
+    `;
+  }
 
   show(imageUrl: string, backgroundImageUrl: string) {
     this.imageUrl = imageUrl;
